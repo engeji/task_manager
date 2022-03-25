e1b87f6f8998fbf43d27853b5bd1032c85f877d1
alexandr <korobeyAV@mail.ru>
2022-03-25 12:05:33 +0500

    done

--- pyTelegramBotAPI.py
+++ pyTelegramBotAPI.py
@@ -1,21 +1,26 @@
+from ctypes import resize
 from traceback import print_tb
 import telebot
-from telebot import apihelper
+from telebot import types
+# from telebot import apihelper
 
-apihelper.proxy = {'http':'socks4://A_korobeynikov:vfhn2022@10.5.95.99:3128'}
+# apihelper.proxy = {'http':'socks4://A_korobeynikov:vfhn2022@10.5.95.99:3128'}
 
 TOKEN='5238123062:AAG7hOcMjGbR6xoGTheEceJBhBP17bjQ2mM'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(m):
-    msg = bot.send_message(m.chat.id, 'Привет!' )
+    msg = bot.send_message(m.chat.id, 'женялох!' )
 
-# bot.polling(timeout=1)
+@bot.message_handler(commands=['button'])
+def button_message(m):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Кнопка')
+    markup.add(item1)
+    bot.send_message(m.chat.id, 'press button', reply_markup=markup)
 
-import requests
-url = f"http://api.telegram.org/bot{TOKEN}/getMe"
-req = requests.get(url, proxies={'http':'http://A_korobeynikov:vfhn2022@10.5.95.99:3128'})
-print(req) 
-print(req.status_code) 
+bot.infinity_polling()
+
+
 
